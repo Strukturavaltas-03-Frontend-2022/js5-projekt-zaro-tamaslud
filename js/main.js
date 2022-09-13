eebf8f797c686f9ec1db23e6fe5c3bf2c19d549f
tamaslud <tamaslud@yahoo.com>
2022-09-13 17:48:55 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict'
+
+/*
+const url = 'http://localhost3000/users';
+
+const fetchData = async (url, options) => {
+  try {
+    const response = await fetch(url, options);
+    if (!response, ok) {
+      throw new Error ('HTTP error' + response.statusText)
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error('Error:' + err.message);
+  }
+};
+*/
