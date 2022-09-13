529f54350b7118f6f5ce2b27a2df9c141a9d565f
tamaslud <tamaslud@yahoo.com>
2022-09-13 19:00:51 +0200

    font added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,24 @@
 'use strict'
 
-/*
-const url = 'http://localhost3000/users';
+const usersUrl = 'http://localhost3000/users';
 
+function handleResult(response) {
+  console.log(response);
+}
+
+async function request(url, options = {}) {
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    return result.users;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+request(usersUrl).then(handleResult)
+
+/*
 const fetchData = async (url, options) => {
   try {
     const response = await fetch(url, options);
