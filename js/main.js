f1b9bad8db568f006010e9c952911ca0bf09e9ac
tamaslud <tamaslud@yahoo.com>
2022-09-18 15:43:18 +0200

    del buttons active

--- js/main.js
+++ js/main.js
@@ -35,10 +35,11 @@ const fetchData = async (url, options) => {
 };
 
 const displayUsersData = (usersData) => {
+  document.querySelector('table .usersData').innerHTML = '';
   for (let i = 0; i < usersData.length; i += 1) {
     const tableRow = `
   <tr>
-  <td>${usersData[i].id}</td>
+  <td class='id'>${usersData[i].id}</td>
   <td>${usersData[i].name}</td>
   <td>${usersData[i].email}</td>
   <td>${usersData[i].address}</td>
@@ -51,9 +52,39 @@ const displayUsersData = (usersData) => {
     document.querySelector('tbody').insertAdjacentHTML('beforeend', tableRow);
   }
 };
+const addButtonListeners = () => {
+  const editButtons = document.querySelectorAll('.editBtn');
+  const delButtons = document.querySelectorAll('.delBtn');
+  for (let i = 0; i < editButtons.length; i += 1) {
+    const id = editButtons[i].parentElement.parentElement.fi;
+    editButtons[i].addEventListener('click', () => editUser(id));
+  }
+  for (let i = 0; i < delButtons.length; i += 1) {
+    const id = delButtons[i].parentElement.parentElement.querySelector('.id').innerHTML;
+    delButtons[i].addEventListener('click', () => delUser(id));
+  }
+};
+
+const editUser = (id) => {
+  // update backend
+  // update object
+  console.log('edit id:', id);
+  displayUsersData(usersData);
+};
+
+const delUser = (id) => {
+  // delete from backend
+  const usersUrlDel = `${usersUrl}/${id}`;
+  (async () => {
+    const usersData = await fetchData(usersUrlDel, fetchOptionsDelete);
+    displayUsersData(usersData);
+    addButtonListeners();
+  })();
+};
 
 (async () => {
   const usersData = await fetchData(usersUrl, fetchOptionsRead);
   console.log(usersData);
   displayUsersData(usersData);
+  addButtonListeners();
 })();
